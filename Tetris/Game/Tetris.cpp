f1ee9032634a3795dc968db5f9bce96e5fbb958d
David-Ks <davidkostandyan71@gmail.com>
2022-09-21 10:14:09 +0400

    Make pretty

---
+++ Tetris/Game/Tetris.cpp
@@ -0,0 +1,42 @@
+#include "Settings.hpp"
+
+#include "Figures/StandardFigure.cpp"
+#include "Map/StandardBoard.cpp"
+#include "Graphics/NcursesWindow.cpp"
+#include "Graphics/StdWindow.cpp"
+#include "GameLoop/StdLoop.cpp"
+#include "Players/StandardPlayer.cpp"
+#include "Events/GameEvent.cpp"
+#include "Events/MenuEvent.cpp"
+
+class Tetris
+{
+private:
+    Draw::Window *window;
+    Map::Board *map;
+    Player *player;
+    Loop *loop;
+
+public:
+    Tetris()
+    {
+        window = new Settings::CustomWindow;
+        player = new Settings::CustomPlayer;
+        loop = new Settings::CustomLoop(player);
+    }
+    ~Tetris()
+    {
+        delete window;
+        delete loop;
+        delete map;
+    }
+
+public:
+    void run()
+    {
+        Listen::MenuEvent *event = new Listen::MenuEvent(window);
+        loop->menu(window, event);
+
+        delete event;
+    }
+};
--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,13 +1,4 @@
-#include "Tetris.cpp"
-
-#include "Game/Figures/StandardFigure.cpp"
-#include "Game/Map/StandardBoard.cpp"
-#include "Game/Graphics/NcursesWindow.cpp"
-#include "Game/Graphics/StdWindow.cpp"
-#include "Game/GameLoop/StdLoop.cpp"
-#include "Game/Players/StandardPlayer.cpp"
-#include "Game/Events/GameEvent.cpp"
-#include "Game/Events/MenuEvent.cpp"
+#include "Game/Tetris.cpp"
 
 int main()
 {
